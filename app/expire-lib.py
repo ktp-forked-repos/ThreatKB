d7a3bb1e69a7cc70a84d430fd361951200d2f843
Adam Musciano <amusciano@gmail.com>
2019-01-10 13:21:58 -0500

    started structure for expirelib file for handling expirations

---
+++ app/expire-lib.py
@@ -0,0 +1,19 @@
+
+import datetime
+
+class expirelib():
+
+    mysql_user = ""
+    mysql_pass = ""
+    mysql_db= "threatkb"
+
+    def isExpired(self):
+        return None
+
+
+    def deleteExpired(self):
+        return None
+
+
+test = expirelib()
+
