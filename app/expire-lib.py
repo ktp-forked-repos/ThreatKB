84dbd673766ec94702ad0ab3bf79d2aa560c4bb0
Adam Musciano <amusciano@gmail.com>
2019-01-12 12:35:05 -0500

    Added expiration function and structure for sql connection

--- app/expire-lib.py
+++ app/expire-lib.py
@@ -1,19 +1,43 @@
+# select id, date_created, expiration_timestamp from c2ip;
+#    +----+---------------------+----------------------+
+#    | id | date_created        | expiration_timestamp |
+#    +----+---------------------+----------------------+
+#    |  1 | 2019-01-10 20:03:02 | 2019-01-10 21:03:02  |
+#    +----+---------------------+----------------------+
+#    1 row in set (0.00 sec)
 
-import datetime
+from datetime import datetime
+from sqlalchemy import select
 
 class expirelib():
+    def __init__(self,dbusr,dbpass,dbport,dbtable="threatkb",dbhost="127.0.0.1"):
+        self.mysql_user = dbusr
+        self.mysql_pass = dbpass
+        self.mysql_db= dbtable
+        self.mysql_host= dbhost
+        self.mysql_port= dbport
 
-    mysql_user = ""
-    mysql_pass = ""
-    mysql_db= "threatkb"
+    def isExpired(self,expired):
+        
+        expiredDate=datetime.strptime(expired,'%Y-%m-%d %H:%M:%S')
+        #print(expiredDate)
+        #print(datetime.now())
+        if(datetime.now() > expiredDate):
+            #print("expired")
+            return True
+        else:
+            #print("not expired")
+            return False
+        
 
-    def isExpired(self):
-        return None
-
+    def displaySQL(self):
+        print(self.mysql_user+"@"+self.mysql_host+":"+self.mysql_port+" ("+self.mysql_db+")")
 
     def deleteExpired(self):
         return None
 
 
-test = expirelib()
+test = expirelib("threatkb","P@sswordsRC00l","3307")
+test.displaySQL()
 
+test.isExpired("2019-01-10 21:03:02")
