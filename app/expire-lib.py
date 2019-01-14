1f99857d4b2381b62e6c2100022dc282e8f9330c
Adam Musciano <amusciano@gmail.com>
2019-01-14 15:54:15 -0500

    Add and handle connections over backgrounded celery task -- need to handle proper importing of db and model instead of using raw sql

--- app/expire-lib.py
+++ app/expire-lib.py
@@ -5,39 +5,58 @@
 #    |  1 | 2019-01-10 20:03:02 | 2019-01-10 21:03:02  |
 #    +----+---------------------+----------------------+
 #    1 row in set (0.00 sec)
+#
+#  Action when expired
+# UPDATE c2ip SET state = "Expired" WHERE id=1;
+
 
 from datetime import datetime
-from sqlalchemy import select
+from sqlalchemy.sql import text
+from app import db
+from app.models import c2ip
+
+from datetime import timedelta
+import celery 
 
 class expirelib():
-    def __init__(self,dbusr,dbpass,dbport,dbtable="threatkb",dbhost="127.0.0.1"):
+    def __init__(self,db):
         self.mysql_user = dbusr
         self.mysql_pass = dbpass
         self.mysql_db= dbtable
         self.mysql_host= dbhost
         self.mysql_port= dbport
+        self.CELERYBEAT_SCHEDULE = {
+                "runs-every-30-seconds":{
+                    "task":"tasks.add",
+                    "schedule": timedelta(seconds=30),
+                    "args":(16,16)
+                },
+            }
+
 
     def isExpired(self,expired):
-        
         expiredDate=datetime.strptime(expired,'%Y-%m-%d %H:%M:%S')
-        #print(expiredDate)
-        #print(datetime.now())
-        if(datetime.now() > expiredDate):
-            #print("expired")
-            return True
-        else:
-            #print("not expired")
-            return False
-        
+        return datetime.now() > expiredDate
 
-    def displaySQL(self):
-        print(self.mysql_user+"@"+self.mysql_host+":"+self.mysql_port+" ("+self.mysql_db+")")
+    def _expireC2(self, conn, id):
+        #UPDATE c2ip SET state = "Expired" WHERE id=1;
+        conn.execute("""UPDATE c2ip SET state = "Retired" WHERE id="""+str(id))
 
-    def deleteExpired(self):
-        return None
+    @celery.task
+    def ExpirationDaemon(self):
+         #select id, date_created, expiration_timestamp from c2ip;
+         connection = db.connect()
+         
+         result = connection.execute("SELECT id, expiration_timestamp FROM c2ip")
+         for row in result:
+             if(row["expiration_timestamp"] is not None):
+                 if(self.isExpired(row["expiration_timestamp"])):
+                     self._expireC2(connection,row["id"])
 
 
 test = expirelib("threatkb","P@sswordsRC00l","3307")
-test.displaySQL()
 
-test.isExpired("2019-01-10 21:03:02")
+if(test.isExpired("2019-01-10 21:03:02")):
+    print("Expired")
+else:
+    print("Not Expired")
